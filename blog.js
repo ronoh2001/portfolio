551ebc6b6e9243b2f648578f7cf2275587740939
ron oh <ronoh2001@gmail.com>
2016-01-14 18:59:55 -0800

    initial init

--- blog.js
+++ blog.js
@@ -5,5 +5,6 @@ var rawData = [
     author:      'Ron Oh',
     authorUrl:   'http://www.ronaldoh.com',
     publishedOn: '2016-01-09',
-    body:        '<p>This is my blog post.  </p>'
+    body:        '<p>This is my blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla</p>'
   },
+];
