5dfb68c50f0c55fbbeacaa6e7fc6e6fbedeaab80
ron oh <ronoh2001@gmail.com>
2016-02-02 20:52:48 -0800

    commit branch

--- blog.js
+++ blog.js
@@ -5,6 +5,6 @@ var rawData = [
     author:      'Ron Oh',
     authorUrl:   'http://www.ronaldoh.com',
     publishedOn: '2016-01-09',
-    body:        '<p>This is my blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla</p>'
+    body:        '<p>Please allow me to introduce myself.  My name is Ron Oh, I am a full time student, parent and General Manager of the Holiday Inn Express.  I am currently enrolls in Code Fellows 301 taking evening classes.</p> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p> '
   },
 ];
